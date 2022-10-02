8b14888b55fd8163f8d880d4068a68dd3a1acf5e
PSungChul <sksh0000@naver.com>
2022-10-03 03:43:26 +0900

    Day09, 계산기(Calc) 추가

---
+++ Day09/src/main/java/EX2_object_parameter.java
@@ -0,0 +1,29 @@
+class Phone {
+	String brand;
+	
+	Phone(String brand) {
+		this.brand = brand;
+	}
+	
+	void getBrand() {
+		System.out.println(brand);
+	}
+}
+
+class Person {
+	String name = "sungchul";
+	
+	void buyPhone(Phone p) {
+		System.out.println("제 명의(" + name + ")로" + p.brand + "폰을 샀습니다.");
+	}
+}
+
+public class EX2_object_parameter {
+	public static void main(String[] args) {
+		Phone phone = new Phone("Samsong");
+		phone.getBrand();
+		
+		Person person = new Person();
+		person.buyPhone(phone);
+	}
+}
