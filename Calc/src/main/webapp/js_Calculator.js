e3b11ea10688d652ffc7635d9cb808edd0df0574
PSungChul <sksh0000@naver.com>
2022-10-04 15:55:54 +0900

    설정값 수정

--- Calc/src/main/webapp/js_Calculator.js
+++ Calc/src/main/webapp/js_Calculator.js
@@ -9,11 +9,7 @@ btn_1.addEventListener("click", function() {
 	// 지역 상수
 	const num_1 = parseInt(document.getElementById("num_1").value);
 	const num_2 = parseInt(document.getElementById("num_2").value);
-	if(num_1 == float(num_1) || num_2 == float(num_2)) {
-		result.innerText("소수값은 입력 될 수 없습니다.")
-	} else {
-		result.innerText = (num_1+num_2);	
-	}
+	result.innerText = (num_1+num_2);
 })
 // 빼기
 btn_2.addEventListener("click", function() {
