d4815e55defa8fd8a2818a86337700df602470a4
PSungChul <sksh0000@naver.com>
2022-10-07 17:40:28 +0900

    sql 추가

--- Day10/src/sample.sql
+++ Day10/src/sample.sql
@@ -1,3 +1,8 @@
+// 학원 : cd C:\Program Files\MySQL\MySQL Server 8.0\bin
+// 집집 : cd 
+
+// MySQL 로그인 : mysql -u root -p
+
 CREATE DATABASE Hotel; // 데이터베이스 생성
 show databases; // 데이터베이스 확인
 USE 데이터베이스명 // 데이터베이스 사용
@@ -8,6 +13,8 @@ CREATE TABLE 테이블명 // 테이블 생성
 	필드이름 필드타입,
 	...
 );
+필드(열) = 클래스 내부
+행 = 클래스 내부 메서드(함수) = 객체
 CREATE TABLE Test
 (
     ID INT,
@@ -48,7 +55,7 @@ VALUES (6, '홍길동', '2016-04-04', 2022),
 
 SELECT 열이름1, 열이름2, …
 FROM 테이블명 --> 전체 행
-WHERE 열이름1="값", 열이름2="값"; --> 조건 // 조건에 만족하는 행
+WHERE 열이름1="값", 열이름2="값", ...; --> 조건 // 조건에 만족하는 행
 // 해당 테이블에서 행을 조회할 수 있는 값/병위
 
 // 예제3
@@ -91,4 +98,67 @@ WHERE Season='fall';
 
 INSERT INTO Room
 VALUES (302, 8, 'fall');
+------------------------------------------
+RENAME TABLE Test TO Reservation; // 테이블 이름 변경
+
+CREATE TABLE Customer
+(
+	ID INT,
+	Name VARCHAR(30),
+	Age INT,
+	Address VARCHAR(30)
+);
+DELETE FROM Customer;
+INSERT INTO Customer
+VALUES (1, '홍길동', 17, '서울');
+INSERT INTO Customer
+VALUES (2, '임꺽정', 11, '인천');
+INSERT INTO Customer
+VALUES (3, '장길산', 13, '서울');
+INSERT INTO Customer
+VALUES (4, '전우치', 17, '수원');
+INSERT INTO Customer
+VALUES (5, '이순신', 20, '목포');
+
+// autocommit 방식
+SHOW VARIABLES LIKE 'autocommit'; // 자동저장 상태 확인
+SET AUTOCOMMIT = FALSE;
+SET AUTOCOMMIT = TRUE;
+COMMIT; // 수동저장
+
+// 제약 조건 NULL값 예제
+INSERT INTO Customer (id, name, address)
+VALUES (6, '김송아', '부산');
+INSERT INTO Customer (id, name, age)
+VALUES (7, '박은빈', 25);
+INSERT INTO Customer (id)
+VALUES (8);
+
+// 제약 조건 추가
+CREATE TABLE mbti
+(
+	idx INT UNIQUE --> PRIMARY KEY,
+	pid INT NOT NULL,
+	name VARCHAR(30),
+	result VARCHAR(30)
+);
+
+INSERT INTO mbti
+VALUES (1, 000101, '김철수', 'INFP');
+// idx가 겹칠경우 - UNIQUE
+INSERT INTO mbti
+VALUES (1, 111111, '김영희', 'ESTJ');
+// pid가 없을 경우 - NOT NULL
+INSERT INTO mbti (idx, name, result)
+VALUES (2, '김영희', 'ESTJ');
+// ALTER 사용법
+// 새로운 컬럼을 생성하면서 제약조건을 같이 걸때 사용
+ALTER TABLE mbti
+ADD idx INT PRIMARY KEY; -- 새로운 컬럼 생성 + (제약조건)
+// 이미 걸려있는곳을 수정할때 사용
+ALTER TABLE mbti
+MODIFY COLUMN idx INT PRIMARY KEY;
+// 미션 : 특정 컬럼 제약 조건 삭제
+ALTER TABLE mbti
+DROP NOT NULL;
 
